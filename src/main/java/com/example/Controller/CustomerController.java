d6a00d8568a894f7be7d19378444df3d85110a8c
Milton Figueredo Miles <gratno@gmail.com>
2024-03-11 16:44:25 -0500

    add method recuperated Key value of the User and session

--- src/main/java/com/example/Controller/CustomerController.java
+++ src/main/java/com/example/Controller/CustomerController.java
@@ -2,11 +2,17 @@ package com.example.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("v1")
@@ -30,8 +36,28 @@ public class CustomerController {
     @GetMapping("/session")
     public ResponseEntity<?> getDetailSession()
     {
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals();
+
+        for (Object session : sessions )
+        {  if(session instanceof  User)
+            {
+                userObject = (User) session;
+            }
+
+             List<SessionInformation> sessionInformations = sessionRegistry.getAllSessions(session, false);
+
+            for(SessionInformation sessionInformation : sessionInformations  )
+            {
+                sessionId = sessionInformation.getSessionId();
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("Response", "Hello Word");
+
         return (ResponseEntity<?>) ResponseEntity.ok();
     }
-
-
 }
