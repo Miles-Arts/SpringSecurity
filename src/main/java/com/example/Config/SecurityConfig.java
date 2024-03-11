309173b9840ba8c338c0fa7aaec52990cd39b34b
Milton Figueredo Miles <gratno@gmail.com>
2024-03-10 22:14:19 -0500

    create other method security Http an init session send onther ip

--- src/main/java/com/example/Config/SecurityConfig.java
+++ src/main/java/com/example/Config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +29,7 @@ public class SecurityConfig {
     }*/
 
     //CONFIGURATION TWO
-    @Bean
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception
     {
         return httpSecurity
@@ -38,6 +40,54 @@ public class SecurityConfig {
                 .formLogin().permitAll()
                 .and()
                 .build();
+    }*/
+
+    /*//CONFIGURATION other redireccionar
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception
+    {
+        return httpSecurity
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/v1/index2").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin()
+                .successHandler(successHandler())
+                .permitAll()
+                .and()
+                .build();
+    }
+
+    public AuthenticationSuccessHandler successHandler()
+    {
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/v1/index");
+        });
+    }*/
+
+    //CONFIGURATION the SESSION
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception
+    {
+        return httpSecurity
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/v1/index2").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin()
+                    .successHandler(successHandler())//URL redireccionar usuario, después de iniciar SESSION
+                    .permitAll()
+                .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)//Diferent Options: ALWAYS- If_REQUIRED - NEVER - STATELESS
+                .and()
+                .build();
+    }
+    public AuthenticationSuccessHandler successHandler()
+    {
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/v1/index");
+        });
     }
 
 }
